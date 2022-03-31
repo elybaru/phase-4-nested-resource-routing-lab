6b7cd11c116c1a9cfbd661efc3c6db16603e8c7b
Ely Karr <elykarr@gmail.com>
2022-03-31 12:09:43 -0700

    commit tree d930ac0a03c51b4725cf08d54885e6c70c6b5d38

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,8 +1,40 @@
 class ItemsController < ApplicationController
+  rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
+  rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity_response
 
   def index
-    items = Item.all
+    if params[:user_id]
+      user = User.find(params[:user_id])
+      items = user.items
+    else
+      items = Item.all
+    end
     render json: items, include: :user
+  end
+
+  def show
+    if params[:id]
+      item = Item.find(params[:id])
+    end
+    render json: item
+  end
+
+  def create
+    user = User.find(params[:user_id])
+    item = user.items.create!({name: params[:name], description: params[:description], price: params[:price]})
+    render json: item, status: :created
+  end
+
+  private
+
+ 
+
+  def render_not_found_response
+    render json: { error: "Item not found" }, status: :not_found
+  end
+
+  def render_unprocessable_entity_response(invalid)
+    render json: { errors: invalid.record.errors }, status: :unprocessable_entity
   end
 
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,20 @@
 class UsersController < ApplicationController
+  rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
+  rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity_response
 
   def show
     user = User.find_by(id: params[:id])
     render json: user, include: :items
+  end
+
+  private
+
+  def render_not_found_response
+    render json: { error: "User not found" }, status: :not_found
+  end
+
+  def render_unprocessable_entity_response(invalid)
+    render json: { errors: invalid.record.errors }, status: :unprocessable_entity
   end
 
 end
